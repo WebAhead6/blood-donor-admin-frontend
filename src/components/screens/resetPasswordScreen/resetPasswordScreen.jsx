46b0cc13ed4c60d26d2dc0e10c2349d148bd8c0c
Tarekb93 <tarekb93@gmail.com>
2020-10-06 11:32:19 +0300

    registerPage

--- src/components/screens/resetPasswordScreen/resetPasswordScreen.jsx
+++ src/components/screens/resetPasswordScreen/resetPasswordScreen.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import ResetPassword from "../../general/resetPassword";
+import backEndApiCall from "../../../utils/backEndApiCall";
+
 import "./resetPasswordScreen.css"
 
 function ResetPasswordScreen() {
+    const [adminResetData, setAdminResetData] = React.useState({email:"", error:""});
+    const handleClick = () => {
+        backEndApiCall("POST",`/resetPassword`,{emailAddress: adminResetData.email})
+          .then((data) => {
+            console.log(data);
+            setAdminResetData({...adminResetData ,error: value})
+          })
+          .catch(console.error);
+      };
     return (
         <div className="resetPassPage">
-            <ResetPassword/>
+            <ResetPassword
+            email={adminResetData.email}
+            onEmailChange={value => setAdminResetData({...adminResetData ,email: value})}
+            handleClick={handleClick} 
+            error = {error}
+            />
+            
         </div>
     )
 }
