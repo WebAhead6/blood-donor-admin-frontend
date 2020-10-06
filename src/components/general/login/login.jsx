e3d7f82987d1293a6fbcafec5b30bb20ac7d0e12
Tarekb93 <tarekb93@gmail.com>
2020-10-06 09:28:21 +0300

    login logic

--- src/components/general/login/login.jsx
+++ src/components/general/login/login.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./login.css";
 import {Link} from "react-router-dom";
+import {routes} from "../../../constants"
 
-const Login = function ({email,password,handleClick}) {
+const Login = function ({email,password,handleClick,onEmailChange,onPasswordChange}) {
 
-    const [adminDetails, setAdminDetails] = React.useState({
-      email:"",
-      password:"",
-    })
+  
 
     return (
 
@@ -15,14 +13,14 @@ const Login = function ({email,password,handleClick}) {
   <div className="login_container">
   <img src="/img/img-logo.svg" alt="" className="login_logo"/>
   <h2 className="login_formTitle">Admin Login</h2>
-  <form action="/login" method="post" className="login_form">
+  <form action="#" className="login_form" onSubmit={(e)=> {e.preventDefault(); handleClick()}}>
       <div className="login_form_inputs">
-          <input className="input" type="email" placeholder="email..." name="email" value={adminDetails.email} onChange={(e) => setAdminDetails({password, email: e.target.value})}/>
-          <input className="input" type="password" placeholder="password" name="password" value={adminDetails.password} onChange={(e) => setAdminDetails({email, password: e.target.value})}/>
+          <input className="input" type="email" placeholder="email..." name="email" value={email} onChange={(e) => onEmailChange (e.target.value)}/>
+          <input className="input" type="password" placeholder="password" name="password" value={password} onChange={(e) => onPasswordChange(  e.target.value)}/>
       </div>
-<input className="input" type="submit" value="" onClick={() => handleClick(setAdminDetails)}/> 
+<input className="input" type="submit" value="" /> 
  </form>
- <Link to="/resetPasswordScreen">
+ <Link to={routes.ResetPasswordScreen}>
  <button className="login_forgotPass"> Forget your password?</button>
  </Link>
  <h2 className="login_error"> Error</h2>
