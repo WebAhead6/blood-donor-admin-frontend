e59e2819d294b7c008e3164e30dd92420e42f20e
hadi kalil <hadikalilweb@gmail.com>
2020-10-06 18:20:11 +0300

    goals screan

--- src/components/general/bloodDropImage/bloodDropImage.jsx
+++ src/components/general/bloodDropImage/bloodDropImage.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import './bloodDropImage.css'
 
-function BloodDropImage() {
+
+function Drop({ scale, text, percentage }) {
+
     return (
-        <div>
-            
+        <div className={`drop_mask drop ${percentage > 100 && 'drop_glow'}`} style={{ transform: `scale(${scale})` }}>
+
+            {/*the content of the drop*/}
+            <div className="drop_mask drop_contentContainer">
+
+                {/*percentage*/}
+                <div className="drop_content">{text}</div>
+
+                {/*the fill of the drop*/}
+                <div className=" drop_fill" style={{ height: `${percentage}%` }}>
+                    {/*wave effect*/}
+                    <div className="drop_wave" />
+                    {/*background wave effect*/}
+                    <div className="drop_wave drop_wave2" />
+                </div>
+            </div>
         </div>
+
     )
 }
 
-export default BloodDropImage
+
+export default Drop
--- src/components/screens/goalsScreen/goalsScreen.jsx
+++ src/components/screens/goalsScreen/goalsScreen.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import "./goalsScreen.css"
+import GolBar from "../../general/goalsBar";
+import Drop from "../../general/bloodDropImage";
+
 
 function GoalsScreen() {
+  //  scale, text, percentage 
+
     return (
-        <div>
-            goal
+        <div className="golScreen">
+            <GolBar />
+            <Drop scale="1" text="" percentage="50" />
         </div>
     )
 }
 
 export default GoalsScreen
+
+
+
+
