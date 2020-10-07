85c28892f83b15a6081e890cebed5135a79f2493
hadi kalil <hadikalilweb@gmail.com>
2020-10-07 17:35:05 +0300

    gols get1

--- src/utils/dist/goals.dev.js
+++ src/utils/dist/goals.dev.js
@@ -56,30 +56,6 @@ var updateGoal = function updateGoal(current, goal) {
       }
     }
   });
-}; //   await db.collection('general').doc('yearly_goals').set(data)
-//   return 'object updated';
-//   const get = async () => {
-//     const goalsRef = await db.collection('general').doc('yearly_goals');
-//     const doc = await goalsRef.get();
-//     if (!doc.exists) {
-//         return new Error('No such document!')
-//     } else {
-//         const data = await doc.data();
-//         return data;
-//     }
-// };
-// const schema = Joi.object({
-//   current: Joi.number()
-//       .integer()
-//       .min(0)
-//       .max(1000000)
-//       .required(),
-//   goal: Joi.number()
-//       .integer()
-//       .min(0)
-//       .max(1000000)
-//       .required()
-// })
-
+};
 
 exports.updateGoal = updateGoal;
