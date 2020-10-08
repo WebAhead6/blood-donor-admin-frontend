32e240f02a44f7d0b99604f864386eb1252803ff
hadi kalil <hadikalilweb@gmail.com>
2020-10-08 16:20:21 +0300

    add butoun component and fix react consept

--- src/components/screens/locationsScreen/locationsScreen.jsx
+++ src/components/screens/locationsScreen/locationsScreen.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./locationsScreen.css";
 import LocationsButton from "../../general/locationsButton/locationsButton"
 
+const exeBackendUrl = process.env.REACT_APP_API
 function LocationsScreen() {
+
+
   return (
     <fieldset className="container">
       <legend>Download for Windows</legend>
-      <LocationsButton href={`/exeFiles/fetchDataFromMada-win.exe`} title={"runs once only"} />
-      <LocationsButton href={"/exeFiles/fetchDataFromMadaEveryHalfHour-win.exe"} title={"runs every half an hour"} />
-      <LocationsButton href={"/exeFiles/fetchDataFromMadaEveryHour-win.exe"} title={"runs every hour"} />
-      <LocationsButton href={"/exeFiles/fetchDataFromMadaEvery2Hours-win.exe"} title={"runs every two hour"} />
-      <LocationsButton href={"/exeFiles/fetchDataFromMadaEvery3Hours-win.exe"} title={"runs every three hour"} />
+      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMada-win.exe`} title={"runs once only"} />
+      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEveryHalfHour-win.exe`} title={"runs every half an hour"} />
+      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEveryHour-win.exe`} title={"runs every hour"} />
+      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEvery2Hours-win.exe`} title={"runs every two hour"} />
+      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEvery3Hours-win.exe`} title={"runs every three hour"} />
     </fieldset>
   );
 }
