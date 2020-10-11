2dfae52dd24934a02e97560910a67ad922c08761
Tarekb93 <tarekb93@gmail.com>
2020-10-11 18:08:20 +0300

    add login Logic

--- src/components/screens/loginScreen/loginScreen.jsx
+++ src/components/screens/loginScreen/loginScreen.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import Login from "../../general/login";
-import backEndApiCall from "../../../utils/backEndApiCall";
 import "./loginScreen.css";
+import { login } from "../../../utils/login";
+
 
 function LoginScreen() {
   const [adminData, setAdminData] = React.useState({email:"",password:""});
  
   const handleClick = () => {
-    backEndApiCall("POST",`/login`,adminData)
-      .then((data) => {
-        console.log(data);
-      
-      })
-      .catch(console.error);
-  };
+  login(adminData.email, adminData.password).then().catch(e => setAdminData({...adminData, error:e.message}))
+  }
   return (
     <div className="loginPage">
       <Login
@@ -22,6 +18,7 @@ function LoginScreen() {
         handleClick={handleClick} 
         onEmailChange={value => setAdminData({...adminData, email: value})}
         onPasswordChange={value => setAdminData({...adminData, password: value})}
+        error = {adminData.error}
       />
     </div>
   );
