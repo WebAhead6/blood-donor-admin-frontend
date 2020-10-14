60676e7a665865c045cc1b90a3ae951171d191f3
Abeer Rizik <abeer.rizik93@gmail.com>
2020-10-14 17:58:26 +0300

    adds rich text type component

---
+++ src/components/general/richTextContextType/richTextContextType.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import RichTextInput from "ra-input-rich-text";
+
+function RichTextContextType() {
+  return (
+    <div>
+      {/* <RichTextInput source="body" validation={{ required: true }} /> */}
+    </div>
+  );
+}
+export default RichTextContextType;
