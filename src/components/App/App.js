bce54dff187e36998aff93f0fe2d7cb802443cd8
Abeer Rizik <abeer.rizik93@gmail.com>
2020-10-14 11:13:53 +0300

    add general setting scren route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 // import constants
-import {routes} from "../../constants"
+import { routes } from "../../constants";
 //general import
 import NavBar from "../general/navBar/navBar.jsx";
 //screen componetnts import
@@ -13,66 +13,69 @@ import LocationsScreen from "../screens/locationsScreen";
 import LoginScreen from "../screens/loginScreen";
 import ResetPasswordScreen from "../screens/resetPasswordScreen";
 import HomeMenuScreen from "../screens/homeMenuScreen";
+import GeneralSettingScreen from "../screens/generalSettingScreen";
 import { useRecoilState } from "recoil";
 import { isLoggedInAtom } from "../../recoilsState";
-import {isLoggedIn as checkLogin}  from "../../utils/login"
+import { isLoggedIn as checkLogin } from "../../utils/login";
 
 import "./App.css";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInAtom)
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInAtom);
   useEffect(() => {
-    checkLogin().then(checkLogedIn => setIsLoggedIn(checkLogedIn))
+    checkLogin().then((checkLogedIn) => setIsLoggedIn(checkLogedIn));
     const intervalId = setInterval(() => {
-      checkLogin().then(checkLogedIn => setIsLoggedIn(checkLogedIn))
-    }, 1000*5) 
-    return ()=> clearInterval(intervalId)
+      checkLogin().then((checkLogedIn) => setIsLoggedIn(checkLogedIn));
+    }, 1000 * 5);
+    return () => clearInterval(intervalId);
   }, []);
-  if (!isLoggedIn) return <div className="App">
-     <Switch>
-         <Route exact path={routes.ResetPasswordScreen}>
+  if (!isLoggedIn)
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path={routes.ResetPasswordScreen}>
             <ResetPasswordScreen />
           </Route>
 
-          <Route  path={routes.home}>
-          <LoginScreen/>
+          <Route path={routes.home}>
+            <LoginScreen />
           </Route>
-            
-    </Switch>
-    </div>
-  return (
-      <div className="App">
-        <NavBar />
-        <Switch>
-          <Route exact path={routes.home}>
-            <HomeScreen />
-          </Route>
-
-          <Route exact path={routes.alertScreen}>
-            <AlertScreen />
-          </Route>
-
-          <Route exact path={routes.ChangePassowrdScreen}>
-            <ChangePassowrdScreen />
-          </Route>
-
-          <Route exact path={routes.GoalsScreen}>
-            <GoalsScreen />
-          </Route>
-
-          <Route exact path={routes.HomeMenuScreen}>
-            <HomeMenuScreen />
-          </Route>
-
-          <Route exact path={routes.LocationsScreen}>
-            <LocationsScreen />
-          </Route>
-
-         
-
-         
         </Switch>
       </div>
+    );
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path={routes.home}>
+          <HomeScreen />
+        </Route>
+
+        <Route exact path={routes.alertScreen}>
+          <AlertScreen />
+        </Route>
+
+        <Route exact path={routes.ChangePassowrdScreen}>
+          <ChangePassowrdScreen />
+        </Route>
+
+        <Route exact path={routes.GoalsScreen}>
+          <GoalsScreen />
+        </Route>
+
+        <Route exact path={routes.HomeMenuScreen}>
+          <HomeMenuScreen />
+        </Route>
+
+        <Route exact path={routes.LocationsScreen}>
+          <LocationsScreen />
+        </Route>
+
+        <Route exact path={routes.GeneralSettingScreen}>
+          <GeneralSettingScreen />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
