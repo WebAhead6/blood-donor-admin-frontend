1d31e9e0382c9ebe9a97376a4882ea006fafbfb1
Abeer Rizik <abeer.rizik93@gmail.com>
2020-10-14 11:14:41 +0300

    add AddentryBar component to GeneralSettinScreen

--- src/components/screens/generalSettingScreen/generalSettingScreen.jsx
+++ src/components/screens/generalSettingScreen/generalSettingScreen.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import AddEntryBar from "../../general/addEntryBar";
 
 function GeneralSettingScreen() {
-  return <div></div>;
+  const [isAdd, setIsAdd] = React.useState(true);
+  return (
+    <div>
+      <AddEntryBar
+        name={"Setting"}
+        isAdd={isAdd}
+        onAddClick={() => setIsAdd(false)}
+        onCancelClick={() => setIsAdd(true)}
+        // onSaveClick={handleSaveClick}
+      />
+    </div>
+  );
 }
 
 export default GeneralSettingScreen;
