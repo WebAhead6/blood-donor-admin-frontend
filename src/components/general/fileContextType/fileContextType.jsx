ffd3516082ef28d0e95af545f2472cb865ede0f8
Abeer Rizik <abeer.rizik93@gmail.com>
2020-10-15 17:22:02 +0300

    convert pdf file to text

--- src/components/general/fileContextType/fileContextType.jsx
+++ src/components/general/fileContextType/fileContextType.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import "./fileContextType.css";
 
+function getBase64(file) {
+  console.log(file);
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      console.log(reader.result);
+      resolve(reader.result);
+    });
+    reader.readAsDataURL(file);
+  });
+}
+
 function FileContextType({ onContextChange, context, canEdit }) {
   const handleContextChange = (value) => {
-    if (canEdit) onContextChange(value);
+    if (canEdit) getBase64(value).then((res) => onContextChange(res));
   };
   return (
     <div className="fileContextType">
-      <label> Upload file </label>
-      <input
-        className="fileContextType-input"
-        type="file"
-        name="file"
-        accept=".pdf"
-        value={context}
-        aria-label="File browser example"
-        onChange={(e) => handleContextChange(e.target.value)}
-      ></input>
+      <a href={context} download="file.pdf">
+        download file
+      </a>
+      {canEdit ? (
+        <>
+          <label> Upload file </label>
+          <input
+            className="fileContextType-input"
+            type="file"
+            name="file"
+            accept=".pdf"
+            required
+            // value={context}
+            aria-label="File browser example"
+            onChange={(e) => handleContextChange(e.target.files[0])}
+          ></input>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
