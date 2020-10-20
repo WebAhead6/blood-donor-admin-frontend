0853336339ed4e877ea80ba572fda6521e66e03a
Abeer Rizik <abeer.rizik93@gmail.com>
2020-10-20 16:46:35 +0300

    fixing the path in locationScreen

--- src/components/screens/locationsScreen/locationsScreen.jsx
+++ src/components/screens/locationsScreen/locationsScreen.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import "./locationsScreen.css";
-import LocationsButton from "../../general/locationsButton/locationsButton"
+import LocationsButton from "../../general/locationsButton/locationsButton";
 
-const exeBackendUrl = process.env.REACT_APP_API
+const exeBackendUrl = process.env.REACT_APP_API;
 function LocationsScreen() {
-
-
   return (
     <fieldset className="container">
       <legend>Download for Windows</legend>
-      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMada-win.exe`} title={"runs once only"} />
-      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEveryHalfHour-win.exe`} title={"runs every half an hour"} />
-      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEveryHour-win.exe`} title={"runs every hour"} />
-      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEvery2Hours-win.exe`} title={"runs every two hour"} />
-      <LocationsButton href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEvery3Hours-win.exe`} title={"runs every three hour"} />
+      <LocationsButton
+        href={`${exeBackendUrl}/exeFiles/fetchDataFromMada.exe`}
+        title={"runs once only"}
+      />
+      <LocationsButton
+        href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEveryHalfHour.exe`}
+        title={"runs every half an hour"}
+      />
+      <LocationsButton
+        href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEveryH exe`}
+        title={"runs every hour"}
+      />
+      <LocationsButton
+        href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEvery2Hours.exe`}
+        title={"runs every two hour"}
+      />
+      <LocationsButton
+        href={`${exeBackendUrl}/exeFiles/fetchDataFromMadaEvery3Hours.exe`}
+        title={"runs every three hour"}
+      />
     </fieldset>
   );
 }
