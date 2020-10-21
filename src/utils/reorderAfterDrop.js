8606509de2d57f9d4d9657d1f4b25ab3ae3522d2
Abeer Rizik <abeer.rizik93@gmail.com>
2020-10-21 18:43:34 +0300

    fixing if statment in reorderAfterDrap

--- src/utils/reorderAfterDrop.js
+++ src/utils/reorderAfterDrop.js
@@ -1,6 +1,6 @@
 const reorderAfterDrap = (arr, sourceIndex, destinationIndex) => {
   // the only one that is required{ destination, source, draggableId }
-  // if (destinationIndex === undefined) return;
+  if (destinationIndex === undefined) return arr;
   const items = Array.from(arr);
 
   const [reorderdItems] = items.splice(sourceIndex, 1);
